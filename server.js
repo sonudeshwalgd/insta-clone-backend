3bed6a8b0741a5cbc1fe7d59beb1098cb5930ba9
sonudeshwalgd <101305714+sonudeshwalgd@users.noreply.github.com>
2023-01-30 23:29:17 +0530

    added post feature after fixes-5

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { router } from "./router/routers.js"
 const app=express()
 app.set("view engine" , "ejs")
 app.use(express.urlencoded({extended:true}))
-app.use("https://insta-backend-ui8r.onrender.com/",router)
+app.use("https://insta-backend-ui8r.onrender.com",router)
 
 
 
